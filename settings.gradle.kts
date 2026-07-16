40c936e6ef1febfba6061cccec3d65acd87e8e28
hamza112-A <hamzakhan68485537@gmail.com>
2026-07-16 14:12:08 +0500

    Add foreground service

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://download.agora.io/sdk/release") }
+    }
+}
+
+rootProject.name = "i220968_i228810"
+include(":app")
